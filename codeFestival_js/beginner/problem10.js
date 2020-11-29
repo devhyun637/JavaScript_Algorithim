16c0300f3ffb610f16330615170afdac97364926
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-11-30 00:03:49 +0900

    feat : 제주 베이스코딩 자바스크립트 초급 문제10 문제풀이

---
+++ codeFestival_js/beginner/problem10.js
@@ -0,0 +1,16 @@
+// 별찍기
+
+const makeTree = (height) => {
+  for (let i = 1; i <= height; i++) {
+    let tree = "";
+    for (let j = 1; j <= height - i; j++) {
+      tree = tree.concat(" ");
+    }
+    for (let j = 1; j < i * 2; j++) {
+      tree = tree.concat("*");
+    }
+    console.log(tree);
+  }
+};
+
+makeTree(5);
