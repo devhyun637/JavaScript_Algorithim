b85d2b2545713003c74605a9bd4eb16dccb40f8a
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-03 00:14:17 +0900

    feat : 제주베이스코딩 문제 18번 문제풀이

---
+++ codeFestival_js/beginner/problem18.js
@@ -0,0 +1,10 @@
+const answer = (string) => {
+  return (
+    string
+      .split(" ")
+      .map(Number)
+      .reduce((value, sum) => value + sum, 0) / 3
+  );
+};
+
+console.log(answer("20 30 40"));
