6679fc933b858f177426a647f42f07e1594d8802
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-04 23:12:22 +0900

    feat : 제주베이스코딩 문제 26번 문제풀이

---
+++ codeFestival_js/beginner/problem26.js
@@ -0,0 +1,16 @@
+const planet = {
+  수성: 'Mercury',
+  금성: 'Venus',
+  지구: 'Earth',
+  화성: 'Mars',
+  목성: 'Jupiter',
+  토성: 'Saturn',
+  천왕성: 'Uranus',
+  해왕성: 'Neptune',
+};
+
+const answer = (string) => {
+  return planet[string];
+};
+
+console.log(answer('지구'));
