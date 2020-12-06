2ee32e68216f079662f37db31392cf2877d96953
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-06 17:09:56 +0900

    fix : 제주베이스 코딩 38번 문제풀이 코드 수정
    - 3위 동점자 수 totalCount에 포함되지 않는 버그 수정

--- codeFestival_js/intermediate/problem38.js
+++ codeFestival_js/intermediate/problem38.js
@@ -7,15 +7,17 @@ const candies = (string) => {
   const answer = [];
   let totalCount = 0;
 
-  while (scores.length > 0 && answer.length < 3) {
+  while (scores.length > 0 && answer.length < 4) {
     const score = scores.pop();
     if (!answer.includes(score)) {
       answer.push(score);
     }
-    totalCount++;
+    if (answer.length !== 4) {
+      totalCount++;
+    }
   }
 
   return totalCount;
 };
 
-console.log(candies('1 2 3 4'));
+console.log(candies('1 1 1 2 2 2 3 3 3 4 4 4'));
