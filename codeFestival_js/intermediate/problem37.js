357bdb30da0c64e2db750274c7491aca29b2dcaa
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-10 21:17:31 +0900

    feat : 제주베이스코딩 문제 37번 문제풀이 추가 구현

--- codeFestival_js/intermediate/problem37.js
+++ codeFestival_js/intermediate/problem37.js
@@ -1,12 +1,29 @@
-const classPresidentElection = (string) => {
-  const result = {};
-  string.split(' ').map((value) => (result[value] ? result[value]++ : (result[value] = 1)));
+function solve1(students) {
+  const votes = {};
+  students
+    .split(' ')
+    .forEach((student) => (votes[student] ? votes[student]++ : (votes[student] = 1)));
 
-  const winner = Object.keys(result).reduce((candidate, current) =>
-    result[candidate] < result[current] ? current : candidate
+  const results = Object.values(votes).map((result) => Number(result));
+  const maxVotes = Math.max(...results);
+  const leader = Object.keys(votes)
+    .filter((value) => maxVotes === votes[value])
+    .join(',');
+
+  return `${leader}(이)가 총 ${maxVotes}표로 반장이 되었습니다.`;
+}
+
+function solve2(students) {
+  const votes = {};
+  students
+    .split(' ')
+    .forEach((student) => (votes[student] ? votes[student]++ : (votes[student] = 1)));
+
+  const leader = Object.keys(votes).reduce((maxVotes, currentVotes) =>
+    votes[currentVotes] > votes[maxVotes] ? currentVotes : maxVotes
   );
+  return `${leader}(이)가 총 ${votes[leader]}표로 반장이 되었습니다.`;
+}
 
-  return `${winner}가 총 ${result[winner]}표로 반장이 되었습니다.`;
-};
-
-console.log(classPresidentElection('원범 원범 혜원 혜원 혜원 혜원 유진 유진'));
+console.log(solve1('원범 원범 혜원 혜원 혜원 혜원 유진 유진'));
+console.log(solve2('원범 원범 혜원 혜원 혜원 혜원 유진 유진'));
