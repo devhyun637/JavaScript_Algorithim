68bfae85833d93c66a80ca3cccc190d66419865b
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2020-12-10 21:34:30 +0900

    feat : 제주베이스코딩 문제 38번 문제풀이 추가 구현

--- codeFestival_js/intermediate/problem38.js
+++ codeFestival_js/intermediate/problem38.js
@@ -20,4 +20,19 @@ const candies = (string) => {
   return totalCount;
 };
 
+const candies2 = (string) => {
+  const scores = {};
+  string
+    .split(' ')
+    .map(Number)
+    .map((score) => (scores[score] ? scores[score]++ : (scores[score] = 1)));
+
+  const studentsCounts = Object.values(scores);
+  return studentsCounts.reduce(
+    (sum, student, index) => (studentsCounts.length - index <= 3 ? sum + student : sum),
+    0
+  );
+};
+
 console.log(candies('1 1 1 2 2 2 3 3 3 4 4 4'));
+console.log(candies2('11 22 33 33 33 44 55 66'));
