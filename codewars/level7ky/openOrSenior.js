565af7dfd6f8185650cb870b488f15fd0973bbca
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-02-13 16:58:44 +0900

    Refactor : remove unnecessary comment

--- codewars/level7ky/openOrSenior.js
+++ codewars/level7ky/openOrSenior.js
@@ -1,6 +1,5 @@
 function openOrSenior(data) {
   // return data.map((person) => (person[0] >= 55 && person[1] > 7 ? 'Senior' : 'Open'));
-  console.log([1, 2, 3, 4, 5].map((value) => [value]));
   return data.map(([age, handicap]) => (age >= 55 && handicap > 7 ? 'Senior' : 'Open'));
 }
 
